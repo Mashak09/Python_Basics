a7620a62a59fe190aae4ffa3986ba315a7d064ad
Mashak Ibrahim <69128375+Mashak09@users.noreply.github.com>
2025-04-29 23:50:48 +0200

    Update Adventure_game.py

--- Adventure_game.py
+++ Adventure_game.py
@@ -1,23 +1,32 @@
-per = input('nindo per endro monu/molu: ')
-print('nindo ee gamel swagatha', per)
+per=input('nindo per endro monu: ')
+print('nindo ee gamel swagatha',per)
 
-answer = input('neen ippa kandatho endl ulle, neen ippa right illeng left pgonu yodeg ponde: ').lower()
-
+answer=input('neen ippa kandatho endl ulle, neen ippa right illeng left pgonu yodeg ponde: ').lower()
 if answer == 'left':
-    answer = input('wah mone, ippa neen pole do awude ulle neen swim akre illeng seedha ponde. neen swim akreng swim illeng walkg walk type aak: ')
+    answer=input('wah mone, ippa neen pole do awude ulle neen swim akre illeng seedha ponde. neen swim akreng swim illeng walkg walk type aak')
 
-    if answer == 'swim':
-        print('nindo mosale thindth, che!!')
-    elif answer == 'walk':
+    if answer=='swim':
+        print('nindo mosale thindth,che!!')
+    elif answer=='walk':
         print('neen walk walk bacchith marche!, che!!')
     else:
         print('nindotta bere endum option ille. Neen sothe!!')
 
-elif answer == 'right':
-    # You need to write something here
-    print('neen right poyi! illa code missing ide!')  # placeholder
+elif answer=='right':
+    print('oru bridge kittith, booldo pole undu neen front ponde back ponde')
+    if answer=='back':
+        print('neen back poye alle adk sothe')
+    elif answer==('front'):
+        print('nikk oru stranger kittra awuldotta mundre ille?')
+
+        if answer=='yes':
+            print('shabas mone! nikk awulu bangar thannal!, neen gendiye!')
+        elif answer=='no':
+            print('aa strrangerg bejar aith neen sothe')
+        else:
+            print('bere option ille, so neen sothe')
 
 else:
-    print('bere option illa, neen sothe!!')
+    print('nindotta bere optioon undutt nirche, sorry you LOSE!!')
 
-print('Danyavadagalu', per)
+print('Thank you for plying this game: ', per)
