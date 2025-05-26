07b9e33356afb631ef4f10c30c895299c579cb5d
Mashak <ibrahimmashak39@gmail.com>
2025-05-26 14:07:54 +0200

    Math_question_generator_update02

--- math_question.py
+++ math_question.py
@@ -12,7 +12,18 @@ def generate_problem():
     left=random.randint(Min_Num,Max_Num)
     right=random.randint(Min_Num,Max_Num)
     # generates two random numbers between the minimum and maximum numbers
-    operators=random.choice(operators)
+    operator=random.choice(operators)
     #randomly selects an operator from the list of operators
-    expr=str(left)+''+operators+''+str(right)
+    expr=str(left)+operator+str(right)
     #creates a string that looks like a mathematical expression ok like 10+20
+    answer=eval(expr)
+    return expr, answer
+    #genrates the expression and the answer to the expression
+    
+wrong=0
+#no of wrong answers given by the user
+input('quiz start madana macha!')
+print('-----------------------')
+#prints the line to indicate the start of the quiz
+start_time=time.time()
+#
