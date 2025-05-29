c0c93c8d00a0b7f08dda3c8a46e651f5ea1839b2
Mashak <ibrahimmashak39@gmail.com>
2025-05-29 12:28:40 +0200

    Turtle game further upgrades

--- turtlegame.py
+++ turtlegame.py
@@ -9,5 +9,18 @@ WIDTH, HEIGHT=400,500
 COLORS = ['red', 'green', 'blue', 'orange', 'yellow', 'black', 'purple', 'pink', 'brown', 'cyan']
 def get_number_of_racers(): #defines a function to get the number of racers
     racers=0
-    While True:
+    while True:
         racers=input('Input the number of racers (2-10):')
+        if racers.isdigit(): #checks if the input is a number
+            racers=int(racers) #converts the inpput to an  integer
+        else:
+            print('input number idonu monu:was tray aak ok')
+            continue
+
+        if  2<= racers <=10:
+            return racers
+        else:
+            print('number 2-10 rangel ille monu. waapas enter aak')
+
+def race(colors):#defines a function called race that takes a list of colors    
+    turtle=create_turtles(colors) #calls the create_turtles function to create turtles with the given color
