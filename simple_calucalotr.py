fd9a213d9665e6d1e405ff0f5a3f174bf663d038
Mashak Ibrahim <ibrahimmashak39@gmail.com>
2025-06-14 15:00:10 +0200

    second update added user can choose between the number of operations

--- simple_calucalotr.py
+++ simple_calucalotr.py
@@ -5,8 +5,26 @@ number2=input('Second Number id monu: ')
 try:
     number1=float(number1)
     number2=float(number2)
-except ValueError: # as told bewfore if the suer enters other than a number shows an error
+except ValueError: # as told before if the user enters other than a number shows an error
     print('Number id monu neen endro ittónthu ulle')
     exit()
 
-operation=input("Yed ayeng oru option choose aak (+, -, *, /): ")
+operation = input("Yed ayeng oru option choose aak (+, -, *, /): ")
+
+if operation == '+':
+    result = number1 + number2
+elif operation == '-':
+    result = number1 - number2
+elif operation == '*':
+    result = number1 * number2
+elif operation == '/':
+    if number2 != 0:
+        result = number1 / number2
+    else:
+        print('divide by 0 awulle monu')
+        exit()
+else:
+    print('nen ewndro operation itro la')
+    exit
+
+
