091c655466ff7fee4db54770f970ffb39aa4d4d0
Mashak Ibrahim <ibrahimmashak39@gmail.com>
2025-06-16 12:54:08 +0200

    inital commit-just added options to choose from

---
+++ expense_tracker.py
@@ -0,0 +1,7 @@
+def show_menu():
+    print("\n==== EXPENSE TRACKER====")
+    print("1.Add Expense")
+    print("View Expense")
+    print("3.Search Expense")
+    print("4.Total Expense")
+    print("5.Exit")
